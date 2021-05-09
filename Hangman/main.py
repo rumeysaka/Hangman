742c84bd4e34a8b9ebca0d85b54a2bdc6b24c606
rumeysaka <73015083+rumeysaka@users.noreply.github.com>
2021-05-09 18:25:29 +0300

    Add files via upload

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,55 +1,34 @@
-
-
 import random
 from words import words
-from hangman_visual import lives_visual_dict
 import string
 
+def Hangman():
+    word=random.choice(words)
+    word_letters=set(word)
+    used_letters=[]
+    print(word)
+    lives=int(input('Enter your desired lives:'))
+    while lives>0 and len(word_letters):
+        guess_letter=input('Guess a letter:')
+        if guess_letter in word_letters:
+            print(guess_letter,' is in the word ')
+            used_letters.append(guess_letter)
+            word_list=[guess_letter if guess_letter in used_letters else "_" for guess_letter in word]
+            word_letters.remove(guess_letter)
+            print(word_list)
+        elif guess_letter not in word_letters:
+            lives-=1
+            if lives>0:
+                print(guess_letter,' is not in the word. Try again, lives remaining= ', lives)
+                used_letters.append(guess_letter)
+                print('Here is the letters you used.' ,used_letters)
 
-def get_valid_word(words):
-    word= random.choice(words)
-    while '-' in word or ' ' in word:
-        word=random.choice(words)
-
-    return word.upper()
-
-def hangman():
-    word = get_valid_word(words)
-    word_letters = set(word)
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()
-    lives = int(input('Enter desired number of lives:'))
-
-    while len(word_letters)>0 and lives>0:
-        print('You have ',lives,' lives left and  you have used these letters:', (used_letters))
-        word_list = [letter if letter in used_letters else '-' for letter in word]
-        print('')
-        print('Current word: \t \t \t \t \t \t \t \t \t', ' '.join(word_list))
-
-        user_letter = input('Guess your letter:').upper()
-
-        if user_letter in alphabet - used_letters:
-            used_letters.add(user_letter)
-            if user_letter in word_letters:
-                word_letters.remove(user_letter)
-
-            else:
-                lives = lives - 1
-                print('\nYour letter,', user_letter, 'is not in the word.')
-
-        elif user_letter in used_letters:
-            lives=lives-1
-            print(f'you have already used {user_letter}. Please guess again.')
-        else:
-            print('you entered invalid character.')
-            used_letters.add(user_letter)
-
-
-    if lives==0:
-        print('you died sorry. The word was', word)
+    if lives == 0:
+        print(guess_letter, ' is not in the word.', used_letters)
+        print('Ran out of lives! GAME OVER ')
     else:
-        print('\t \t \t \t \t \t \t \t \t \t' ,' '.join(word))
-        print('YAY you made it.')
+        print('\t', word.upper())
 
+        print('YAY. You made it.')
 
-hangman()
+Hangman()
